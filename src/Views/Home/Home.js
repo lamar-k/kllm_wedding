ca857ed0f5b3fae18d540ed65e0c191605d38fa8
lamar-k <kenlamar8@gmail.com>
2025-06-15 18:48:13 -0400

    add image on home
    added image on home page of LK

--- src/Views/Home/Home.js
+++ src/Views/Home/Home.js
@@ -4,6 +4,7 @@ import './Home.css';
 import '../../Components/Countdown/Countdown.js';
 import Countdown from "../../Components/Countdown/Countdown.js";
 import Navigation from '../../Components/Navigation/Navigation.js';
+import LKIMAGE from "../../images/LK_HOME.png";
 
 function Home() {
     return (
@@ -15,11 +16,17 @@ function Home() {
             </div>
             <div className="header-text">
                <h2 style={{color:'black'}} className="script_text">Lauren + Ken</h2>
-               <h3 style={{color:'black'}} className="script_subtext">June 13th 2026 &middot; Washington D.C.</h3>
+               <h3 style={{color:'black'}} className="script_subtext">June 13, 2026 &emsp; | &emsp; Washington, D.C.</h3>
             </div>
             <Countdown/>
 
             <Navigation/>
+
+            <div style={{display:'flex', justifyContent:'center',  marginTop:'20px'}}>
+            <img src={LKIMAGE} alt="LK Wedding Photo" className="LK"
+                style={{display:'flex', width:'90%', maxHeight:'70vh', objectFit:'contain', marginBottom:'10px', marginTop:'10px'}} 
+                />
+            </div>
 
         </div>
     );
