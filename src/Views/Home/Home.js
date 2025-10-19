11f5fba8422ef1837e4ff8d944073198f4109da7
lamar-k <kenlamar8@gmail.com>
2025-10-18 22:54:02 -0400

    add modal for rsvp button
    create modal for base of searching for user name to rsvp

--- src/Views/Home/Home.js
+++ src/Views/Home/Home.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 import './Home.css';
 import '../../Components/Countdown/Countdown.js';
 import Countdown from "../../Components/Countdown/Countdown.js";
 import Navigation from '../../Components/Navigation/Navigation.js';
 import LKIMAGE from "../../images/LK_HOME.png";
-import {Button } from "@mui/material";
+import {Button, Typography, Modal, Box, TextField, IconButton, InputAdornment} from "@mui/material";
+import CloseIcon from '@mui/icons-material/Close';
+import SearchIcon from '@mui/icons-material/Search';
 
 
 function Home() {
+    const [rsvpOpen, setRsvpOpen] = useState(false);
+    const handleRSVPOpen = () => setRsvpOpen(true);
+    const handleRSVPClose = () => setRsvpOpen(false);
+
+    const [modalHeight, setModalHeight] = useState(window.innerHeight * 0.2);
+    const [modalWidth, setModalWidth] = useState(window.innerWidth * 0.2);
+
     return (
         <div className="home">
             <div className="header-text">
@@ -17,12 +26,55 @@ function Home() {
             <Countdown/>
 
             <div>
-            <Button variant="outlined" sx={{color:'black', 
+            <Button variant="outlined" onClick={handleRSVPOpen} sx={{color:'black', 
                     transition: 'box-shadow 0.3s ease-in-out', borderColor:'black'
                     , '&:hover': {color:'white', borderColor:'white', backgroundColor:'black'}}}>
                         RSVP HERE
                     </Button>
             </div>
+
+            <Modal
+                open={rsvpOpen}
+                onClose={handleRSVPClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+                style={{ backdropFilter: "blur(5px)" }}>
+                <Box sx={{
+                    position: 'absolute',
+                    top: '50%',
+                    left: '50%',
+                    transform: 'translate(-50%, -50%)',
+                    width: modalWidth,
+                    height: modalHeight,
+                    bgcolor: 'background.paper',
+                    boxShadow: 24,
+                    p: 4,
+                }}>
+                    <div style={{flex:1, justifyContent:'right', textAlign:'right'}}>
+                        <CloseIcon onClick={handleRSVPClose} sx={{'&:hover': {cursor:'pointer'}}}/>
+                    </div>
+                    <div style={{flex:1, justifyContent:'center', textAlign:'center', marginTop:'25px'}}>
+                    <Typography id="modal-modal-title" variant="h6" component="h2">
+                        RSVP by searching your full name:
+                    </Typography>
+                    <TextField label="Search Full Name"
+            variant="outlined"
+            // value={searchTerm}
+            // onChange={handleChange}
+            fullWidth
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton type="submit" aria-label="search">
+                    <SearchIcon />
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+            style={{marginTop:'25px'}}/>
+                    </div>
+                </Box>
+            </Modal>
 
             <Navigation/>
 
